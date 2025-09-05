d765ca5c8389a90e428ba7887733b6155efbb639
amiramiour <amiouramirtahar@gmail.com>
2025-09-05 12:02:03 +0200

    auth modifs

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart' as fb;
+import '../../domain/entities/app_user.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../datasources/firebase_auth_datasource.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final FirebaseAuthDataSource _ds;
+  AuthRepositoryImpl(this._ds);
+
+  AppUser? _map(fb.User? u) => u == null ? null : AppUser(uid: u.uid, email: u.email);
+
+  @override
+  Stream<AppUser?> authStateChanges() => _ds.authState().map(_map);
+
+  @override
+  AppUser? currentUser() => _map(_ds.currentUser());
+
+  @override
+  Future<AppUser> registerWithEmail(String email, String password) async =>
+      _map(await _ds.register(email, password))!;
+
+  @override
+  Future<AppUser> signInWithEmail(String email, String password) async =>
+      _map(await _ds.signIn(email, password))!;
+
+  @override
+  Future<void> signOut() => _ds.signOut();
+}
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -3,7 +3,7 @@ import '../entities/app_user.dart';
 abstract class AuthRepository {
   Stream<AppUser?> authStateChanges();
   Future<AppUser> signInWithEmail(String email, String password);
-  Future<AppUser> RegisterWithEmail(String email, String password);
+  Future<AppUser> registerWithEmail(String email, String password);
   Future<void> signOut();
   AppUser? currentUser();
 
