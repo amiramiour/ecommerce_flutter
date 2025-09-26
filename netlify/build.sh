252f5d97206ce20d13e00a602abb2c825b74e28f
klmr ✌🏼 <105491169+MaxRichet@users.noreply.github.com>
2025-09-26 15:17:48 +0200

    fix(build): unset FLUTTER_WEB_RENDERER for Flutter 3.35

--- netlify/build.sh
+++ netlify/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Installe Flutter
 git clone https://github.com/flutter/flutter.git -b ${FLUTTER_CHANNEL:-stable} --depth 1 $HOME/flutter
 export PATH="$HOME/flutter/bin:$PATH"
 
 flutter --version
 flutter pub get
 
-# Build Flutter Web
-flutter build web --release --pwa-strategy=none canvaskit
+unset FLUTTER_WEB_RENDERER
+
+flutter build web --release --pwa-strategy=none
